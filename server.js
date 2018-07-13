5e0c3cb3f19f1a2c5a8ce7647b9cb5edca706aa3
rashidmajeed <rashid.caliber@gmail.com>
2018-07-13 21:24:50 +0200

    setting up env variable and connecting mongodb with app

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
 const express = require('express');
+
+const mongoose = require('mongoose');
+require('dotenv').config({ path: 'variables.env'});
+
+// connect to mongodb database
+
+mongoose
+.connect(process.env.MONGO_URI)
+.then(() => console.log('DB is connected with the web application...'))
+.catch(err => console.error(err));
+
+// Intializing application
 const app = express();
 
 const PORT = process.env.PORT || 4444;
