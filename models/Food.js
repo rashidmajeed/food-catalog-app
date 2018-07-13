f35fe3a007e609785ca4ce8d29f61fd7afba6062
rashidmajeed <rashid.caliber@gmail.com>
2018-07-13 22:58:10 +0200

    user and food models with mongoose schema is created

---
+++ models/Food.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+
+const FoodSchema = new Schema({
+
+    name: {
+        type: String,
+        required: true
+    },
+
+    category: {
+        type: String,
+        required: true
+    },
+
+    description: {
+        type: String,
+        required: true
+    },
+
+    instruction: {
+        type: String,
+        required: true
+    },
+
+    createdDate: {
+        type: Date,
+        default: Date.now
+    },
+
+    likes: {
+        type: Number,
+        default: 0
+    },
+
+    username: {
+        type: String
+    }
+})
+
+
+module.exports = mongoose.model('Food', FoodSchema);
