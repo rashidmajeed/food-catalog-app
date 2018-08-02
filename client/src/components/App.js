f1ec1f602640ac01f2950760ce378abfedbcf7bb
rashidmajeed <rashid.caliber@gmail.com>
2018-08-02 22:57:07 +0200

    using create-react-app to implement client side of app with reactjs and apollo client

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Frontend is with react and apollo client</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
